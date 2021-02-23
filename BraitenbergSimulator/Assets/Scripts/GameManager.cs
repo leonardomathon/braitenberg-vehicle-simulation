ef70ec5decb769174e8009005831f8ed70e857a4
leonardomathon <leonardo.mathon@hotmail.com>
2021-02-23 12:32:31 +0100

    Add object selection

--- BraitenbergSimulator/Assets/Scripts/GameManager.cs
+++ BraitenbergSimulator/Assets/Scripts/GameManager.cs
@@ -44,14 +44,6 @@ public class GameManager : MonoBehaviour
 
     #endregion
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            SelectObject();
-        }
-    }
-
     public bool AllowSpawnVehicle()
     {
         return vehicles.Count < maxVehicles;
@@ -60,22 +52,5 @@ public class GameManager : MonoBehaviour
     public bool AllowSpawnLights()
     {
         return lights.Count < maxLights;
-    }
-
-    private void SelectObject()
-    {
-        RaycastHit hit;
-
-        // Shoot a ray from main camera through screen point
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        // Check if ray hits something with the mask "Vehicle"
-        if (Physics.Raycast(ray, out hit, 100))
-        {
-            GameObject hitObject = hit.transform.gameObject;
-            GameObject cameraController = GameObject.Find("Camera Controller");
-            DefaultCameraMovement camera = cameraController.GetComponent<DefaultCameraMovement>();
-            camera.setTarget(hitObject);
-        }
     }
 }
---
+++ BraitenbergSimulator/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject selectedObject;
+
+    public GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SelectObject();
+        }
+    }
+
+    public void ResetSelection()
+    {
+
+    }
+
+    private void SelectObject()
+    {
+        RaycastHit hit;
+
+        // Shoot a ray from main camera through screen point
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Check if ray hits something with the mask "Vehicle"
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+
+            if ((hitObject.GetComponent("Vehicle") as Vehicle) != null)
+            {
+                GameObject cameraController = GameObject.Find("Camera Controller");
+                DefaultCameraMovement camera = cameraController.GetComponent<DefaultCameraMovement>();
+                camera.setTarget(hitObject);
+            }
+
+
+        }
+    }
+}
