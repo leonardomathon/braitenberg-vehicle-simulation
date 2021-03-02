4c2c873816fbeefe730a596b68b8eb7cb7c9dc5f
JorgBekelaar <j.w.r.bekelaar@student.tue.nl>
2021-03-02 17:17:49 +0100

    Implement draggable objects

---
+++ BraitenbergSimulator/Assets/Scripts/DragObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mouseOffset;
+    private float zCoordinate;
+
+    void OnMouseDown()
+    {  
+        // Compute the Z-coordinate of the object
+        zCoordinate = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        
+        // Compute the mouse offset
+        mouseOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, zCoordinate));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, zCoordinate)) + mouseOffset;
+        transform.position = new Vector3(Mathf.Ceil(newPosition.x), transform.position.y, Mathf.Ceil(newPosition.z));
+    }
+}
