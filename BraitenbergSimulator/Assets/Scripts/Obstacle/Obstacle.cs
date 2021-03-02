47440aadd44622c69bd04d703c4bc91d485bd90b
leonardomathon <leonardo.mathon@hotmail.com>
2021-03-02 11:28:24 +0100

    Add obstacle class

---
+++ BraitenbergSimulator/Assets/Scripts/Obstacle/Obstacle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    [SerializeField] private Material materialStandard;
+
+    [SerializeField] private Material materialOnSelect;
+
+    [SerializeField] private bool isSelected;
+
+    public void Select()
+    {
+        isSelected = true;
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
+    }
+
+    public bool IsSelected()
+    {
+        return isSelected;
+    }
+
+    private void ApplySelectionMaterial()
+    {
+        // Apply material for object selection
+        gameObject.GetComponent<MeshRenderer>().sharedMaterial = materialOnSelect;
+    }
+
+    private void RemoveSelectionMaterial()
+    {
+        // Only change if necessary
+        if (gameObject.GetComponent<MeshRenderer>().sharedMaterial.name == materialOnSelect.name)
+        {
+            // Apply default material
+            gameObject.GetComponent<MeshRenderer>().sharedMaterial = materialStandard;
+        }
+    }
+}
