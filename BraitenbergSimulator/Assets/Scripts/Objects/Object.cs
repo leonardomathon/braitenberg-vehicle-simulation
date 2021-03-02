7852f6e85a2ac5ca561f90192d902e7acbd42183
leonardomathon <leonardo.mathon@hotmail.com>
2021-03-02 12:51:58 +0100

    Add comments

--- BraitenbergSimulator/Assets/Scripts/Objects/Object.cs
+++ BraitenbergSimulator/Assets/Scripts/Objects/Object.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class Object : MonoBehaviour
 {
+    // Boolean that stores if object is selected
+    [SerializeField] private bool isSelected;
+
+    // Standard material of object
     [SerializeField] private Material materialStandard;
 
+    // Material when object is selected
     [SerializeField] private Material materialOnSelect;
-
-    [SerializeField] private bool isSelected;
 
     protected virtual void Update()
     {
         if (isSelected)
         {
+            // Apply selection material only if selected
             ApplySelectionMaterial();
-
         }
         else
         {
+            // Remove selection material else
             RemoveSelectionMaterial();
         }
+    }
+
+    // Getter and setters for isSelected
+    public bool IsSelected()
+    {
+        return isSelected;
     }
 
     public void Select()
@@ -33,17 +43,14 @@ public class Object : MonoBehaviour
         isSelected = false;
     }
 
-    public bool IsSelected()
-    {
-        return isSelected;
-    }
-
+    // Applies selection material to this object
     private void ApplySelectionMaterial()
     {
         // Apply material for object selection
         gameObject.GetComponent<MeshRenderer>().sharedMaterial = materialOnSelect;
     }
 
+    // Removes selection material from this object
     private void RemoveSelectionMaterial()
     {
         // Only change if necessary
