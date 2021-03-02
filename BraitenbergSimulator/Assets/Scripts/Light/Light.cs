5ec901550cdf07a5b93702b897c9532ab4544bd6
JorgBekelaar <j.w.r.bekelaar@student.tue.nl>
2021-03-02 11:29:28 +0100

    Create lightsource class

---
+++ BraitenbergSimulator/Assets/Scripts/Light/Light.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    // Intensity of the lightsource
+    [SerializeField]
+    private int lightIntensity;
+
+    // Color of the lightsource
+    [SerializeField]
+    private int lightColor;
+
+    // Boolean indicating if the lightsource is selected
+    [SerializeField]
+    private bool isSelected;
+
+    // Select the lightsource
+    public void Select()
+    {
+        isSelected = true;
+    }
+
+    // Deselect the lightsource
+    public void Deselect()
+    {
+        isSelected = false;
+    }
+
+    // Return the selected state of the lightsource
+    public bool IsSelected()
+    {
+        return isSelected;
+    }
+}
