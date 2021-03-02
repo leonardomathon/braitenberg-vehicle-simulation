2a5b2ede21c4c822312320b3f0fa0ec6ab508007
leonardomathon <leonardo.mathon@hotmail.com>
2021-03-02 12:04:51 +0100

    Add selection texture to lightbulb on select

--- BraitenbergSimulator/Assets/Scripts/Light/Lightbulb.cs
+++ BraitenbergSimulator/Assets/Scripts/Light/Lightbulb.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class Lightbulb : MonoBehaviour
 {
     // Intensity of the lightsource
-    [SerializeField]
-    private int lightIntensity;
+    [SerializeField] private int lightIntensity;
 
     // Color of the lightsource
-    [SerializeField]
-    private int lightColor;
+    [SerializeField] private int lightColor;
+
+    [SerializeField] private Material materialStandard;
+
+    [SerializeField] private Material materialOnSelect;
 
     // Boolean indicating if the lightsource is selected
-    [SerializeField]
-    private bool isSelected;
+    [SerializeField] private bool isSelected;
+
+    void Update()
+    {
+        if (isSelected)
+        {
+            // Apply material
+            ApplySelectionMaterial();
+
+        }
+        else
+        {
+            // Remove selection material
+            RemoveSelectionMaterial();
+        }
+    }
 
     // Select the lightsource
     public void Select()
@@ -32,5 +48,21 @@ public class Lightbulb : MonoBehaviour
     public bool IsSelected()
     {
         return isSelected;
+    }
+
+    private void ApplySelectionMaterial()
+    {
+        // Apply material for object selection
+        gameObject.GetComponent<MeshRenderer>().sharedMaterial = materialOnSelect;
+    }
+
+    private void RemoveSelectionMaterial()
+    {
+        // Only change if necessary
+        if (gameObject.GetComponent<MeshRenderer>().sharedMaterial.name == materialOnSelect.name)
+        {
+            // Apply default material
+            gameObject.GetComponent<MeshRenderer>().sharedMaterial = materialStandard;
+        }
     }
 }
