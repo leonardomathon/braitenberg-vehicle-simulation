5e6d4576dc9b769d7ad7e90d102d9fabfe71caec
leonardomathon <leonardo.mathon@hotmail.com>
2021-03-14 21:05:47 +0100

    Fix update selection

--- BraitenbergSimulator/Assets/Scripts/Objects/Vehicle/Vehicle.cs
+++ BraitenbergSimulator/Assets/Scripts/Objects/Vehicle/Vehicle.cs
@@ -1,87 +1,101 @@
 ﻿using Objects.Vehicle.Motors;
+using Objects.Light;
 using UnityEngine;
+using System.Collections.Generic;
 
-namespace Objects.Vehicle {
-	[System.Serializable] public class Vehicle : Object {
-		[SerializeField] private VehicleType type;
+namespace Objects.Vehicle
+{
+    [System.Serializable]
+    public class Vehicle : Object
+    {
+        [SerializeField] private VehicleType type;
 
-		private VehicleType _type;
+        private VehicleType _type;
 
-		[SerializeField] private Vector3 position;
+        [SerializeField] private Vector3 position;
 
-		public GameObject body;
+        public GameObject body;
 
-		public Wheel leftWheel;
-		public Wheel rightWheel;
+        public Wheel leftWheel;
+        public Wheel rightWheel;
 
-		public Sensor leftSensor;
-		public Sensor rightSensor;
-	
-		private GameManager gameManager;
-		private VehicleMovement movement;
+        public Sensor leftSensor;
+        public Sensor rightSensor;
 
-		private new void Start() {
-			AttachMovementScript();
-		}
+        private GameManager gameManager;
+        private VehicleMovement movement;
 
-		protected void Update() {
-			// base.Update();
-			// UpdateMovementScript();
-			UpdateBodyRotation();
+        private new void Start()
+        {
+            AttachMovementScript();
+        }
 
-			var lights = gameManager.GetLights();
-			var measurements = new[] {leftSensor.Measure(lights), rightSensor.Measure(lights)};
-			var activations = movement.MotorActivation(measurements);
+        protected void Update()
+        {
+            // base.Update();
+            // UpdateMovementScript();
+            UpdateBodyRotation();
 
-			// Debug.Log(activations.Aggregate("Motors: ", (current, activation) => current + (activation + ", ")));
+            List<Lightbulb> lights = gameManager.GetLights();
+            float[] measurements = new float[] { leftSensor.Measure(lights), rightSensor.Measure(lights) };
+            float[] activations = movement.MotorActivation(measurements);
 
-			leftWheel.SetForce(activations[0]);
-			rightWheel.SetForce(activations[1]);
-		}
+            // Debug.Log(activations.Aggregate("Motors: ", (current, activation) => current + (activation + ", ")));
 
-		// Attach the right movement script to the vehicle object based on VehicleType
-		private void AttachMovementScript() {
-			_type = type;
-			SetMovementType(type);
-		}
-		private void SetMovementType(VehicleType type) {
-			switch (type) {
-				case VehicleType.Agression:
-					movement = new VehicleMovementAgression();
-					break;
-				case VehicleType.Exploration:
-					movement = new VehicleMovementExploration();
-					break;
-				case VehicleType.Fear:
-					movement = new VehicleMovementFear();
-					break;
-				case VehicleType.Love:
-					movement = new VehicleMovementLove();
-					break;
-				default:
-					movement = new VehicleMovementDefault();
-					break;
-			}
-		}
+            leftWheel.SetForce(activations[0]);
+            rightWheel.SetForce(activations[1]);
+        }
 
-		// Update movementscript if VehicleType has changed
-		private void UpdateMovementScript() {
-			// TODO: This could and probably should be replaced with some event-like call when the type is actually changed
-			if (type != _type) {
-				_type = type;
-				SetMovementType(type);
-			}
-		}
+        // Attach the right movement script to the vehicle object based on VehicleType
+        private void AttachMovementScript()
+        {
+            _type = type;
+            SetMovementType(type);
+        }
+        private void SetMovementType(VehicleType type)
+        {
+            switch (type)
+            {
+                case VehicleType.Agression:
+                    movement = new VehicleMovementAgression();
+                    break;
+                case VehicleType.Exploration:
+                    movement = new VehicleMovementExploration();
+                    break;
+                case VehicleType.Fear:
+                    movement = new VehicleMovementFear();
+                    break;
+                case VehicleType.Love:
+                    movement = new VehicleMovementLove();
+                    break;
+                default:
+                    movement = new VehicleMovementDefault();
+                    break;
+            }
+        }
 
-		private void UpdateBodyRotation() {
-			var transformLocalRotation = body.transform.rotation;
-			transformLocalRotation.x = 0;
-			transformLocalRotation.z = 0;
-			body.transform.rotation = transformLocalRotation;
-		}
+        // Update movementscript if VehicleType has changed
+        private void UpdateMovementScript()
+        {
+            // TODO: This could and probably should be replaced with some event-like call when the type is actually changed
+            if (type != _type)
+            {
+                _type = type;
+                SetMovementType(type);
+            }
+        }
 
-		public void SetGameManager(GameManager gameManager) {
-			this.gameManager = gameManager;
-		}
-	}
+        private void UpdateBodyRotation()
+        {
+            var transformLocalRotation = body.transform.rotation;
+            transformLocalRotation.x = 0;
+            transformLocalRotation.z = 0;
+            body.transform.rotation = transformLocalRotation;
+        }
+
+        public void SetGameManager(GameManager gameManager)
+        {
+            this.gameManager = gameManager;
+        }
+    }
 }
--- BraitenbergSimulator/Assets/Scripts/SelectionController.cs
+++ BraitenbergSimulator/Assets/Scripts/SelectionController.cs
@@ -5,232 +5,269 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Object = Objects.Object;
 
-public class SelectionController : MonoBehaviour {
-	[SerializeField] private Selectable selectedObject;
+public class SelectionController : MonoBehaviour
+{
+    [SerializeField] private Selectable selectedObject;
 
-	[SerializeField] private bool selectedObjectMovable;
+    [SerializeField] private bool selectedObjectMovable;
 
-	// The layer mask on which we can select objects
-	[SerializeField] private LayerMask selectableAreaMasks;
+    // The layer mask on which we can select objects
+    [SerializeField] private LayerMask selectableAreaMasks;
 
-	// The layer mask on wich we can move object
-	[SerializeField] private LayerMask moveableAreaMask;
+    // The layer mask on wich we can move object
+    [SerializeField] private LayerMask moveableAreaMask;
 
-	private GameManager gameManager;
+    private GameManager gameManager;
 
-	private SoundManager soundManager;
+    private SoundManager soundManager;
 
-	private CameraController cameraController;
+    private CameraController cameraController;
 
-	// Singleton pattern for SelectionController
+    // Singleton pattern for SelectionController
 
-	#region singleton
-	public static SelectionController Instance {get; private set;}
-	
-	private void Awake() {
-		if (Instance != null && Instance != this) {
-			Destroy(gameObject);
-		} else {
-			Instance = this;
-		}
-	}
-	#endregion
+    #region singleton
+    public static SelectionController Instance { get; private set; }
 
-	private void Start() {
-		gameManager = GameManager.Instance;
-		soundManager = SoundManager.Instance;
-		cameraController = CameraController.Instance;
-	}
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    #endregion
 
-	private void Update() {
-		if (!selectedObjectMovable && Input.GetMouseButtonDown(0)) {
-			SelectObject();
-		}
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+        soundManager = SoundManager.Instance;
+        cameraController = CameraController.Instance;
+    }
 
-		if (selectedObject && Input.GetKeyDown(KeyCode.M)) {
-			MoveSelectedObject();
-		}
+    private void Update()
+    {
+        if (!selectedObjectMovable && Input.GetMouseButtonDown(0))
+        {
+            SelectObject();
+        }
 
-		if (selectedObject && Input.GetKeyDown(KeyCode.R)) {
-			RotateSelectedObject();
-		}
+        if (selectedObject && Input.GetKeyDown(KeyCode.M))
+        {
+            MoveSelectedObject();
+        }
 
-		if (selectedObjectMovable) {
-			Move();
-		}
+        if (selectedObject && Input.GetKeyDown(KeyCode.R))
+        {
+            RotateSelectedObject();
+        }
 
-		if (selectedObjectMovable && Input.GetMouseButtonDown(0)) {
-			PlaceSelectedObject();
-		}
+        if (selectedObjectMovable)
+        {
+            Move();
+        }
 
-		// Check if selected object still exists
-		if (selectedObject == null) {
-			selectedObject = null;
-		}
-	}
+        if (selectedObjectMovable && Input.GetMouseButtonDown(0))
+        {
+            PlaceSelectedObject();
+        }
 
-	public Selectable GetSelectedObject() {
-		return selectedObject;
-	}
+        // Check if selected object still exists
+        if (selectedObject == null)
+        {
+            selectedObject = null;
+        }
+    }
 
-	private void ResetSelection() {
-		cameraController.ResetTarget();
-	}
+    public Selectable GetSelectedObject()
+    {
+        return selectedObject;
+    }
 
-	public void DeleteSelectedObject() {
-		// Play object delete sound
-		soundManager.PlayDeleteObjectSound();
+    private void ResetSelection()
+    {
+        cameraController.ResetTarget();
+    }
 
-		// Deselect before destroying
-		DeselectAllObjects();
+    public void DeleteSelectedObject()
+    {
+        // Play object delete sound
+        soundManager.PlayDeleteObjectSound();
 
-		// Cleanup object list, brute force
-		foreach (var vehicle in gameManager.vehicles) {
-			if (vehicle == selectedObject) {
-				gameManager.vehicles.Remove((Vehicle) selectedObject);
-				Destroy(selectedObject);
-				return;
-			}
-		}
-		foreach (var various in gameManager.various) {
-			if (various == selectedObject) {
-				gameManager.various.Remove((Object) selectedObject);
-				Destroy(selectedObject);
-				return;
-			}
-		}
-		foreach (var bulb in gameManager.lights) {
-			if (bulb == selectedObject) {
-				gameManager.lights.Remove((Lightbulb) selectedObject);
-				Destroy(selectedObject);
-				return;
-			}
-		}
-	}
+        // Deselect before destroying
+        DeselectAllObjects();
 
-	public void SetSelectedObject(Selectable selectable) {
-		// Select all objects to make sure only one object is selected
-		DeselectAllObjects();
+        // Cleanup object list, brute force
+        foreach (Objects.Vehicle.Vehicle vehicle in gameManager.vehicles)
+        {
+            if (vehicle == selectedObject)
+            {
+                gameManager.vehicles.Remove(vehicle);
+                Destroy(selectedObject.gameObject);
+                return;
+            }
+        }
+        foreach (Objects.Object various in gameManager.various)
+        {
+            if (various == selectedObject)
+            {
+                gameManager.various.Remove(various);
+                Destroy(selectedObject.gameObject);
+                return;
+            }
+        }
+        foreach (Objects.Light.Lightbulb bulb in gameManager.lights)
+        {
+            if (bulb == selectedObject)
+            {
+                gameManager.lights.Remove(bulb);
+                Destroy(selectedObject.gameObject);
+                return;
+            }
+        }
+    }
 
-		// Play object select sound
-		soundManager.PlaySelectObjectSound();
+    public void SetSelectedObject(Selectable selectable)
+    {
+        // Select all objects to make sure only one object is selected
+        DeselectAllObjects();
 
-		// Set the selected object
-		selectedObject = selectable;
+        // Play object select sound
+        soundManager.PlaySelectObjectSound();
 
-		// Select the object
-		selectable.Select();
+        // Set the selected object
+        selectedObject = selectable;
 
-		// Let camera orbit around selected object
-		cameraController.SetTarget(selectable.gameObject);
-	}
+        // Select the object
+        selectable.Select();
 
-	public void ResetSelectedObject() {
-		// Play object deselect sound
-		soundManager.PlayDeselectObjectSound();
+        // Let camera orbit around selected object
+        cameraController.SetTarget(selectable.gameObject);
+    }
 
-		// Reset the selected object
-		selectedObject = null;
+    public void ResetSelectedObject()
+    {
+        // Play object deselect sound
+        soundManager.PlayDeselectObjectSound();
 
-		// Deselect all objects
-		DeselectAllObjects();
-	}
+        // Reset the selected object
+        selectedObject = null;
 
-	private void SelectObject() {
-		// First check if mouse is currently over UI, do nothing
-		if (EventSystem.current.IsPointerOverGameObject()) return;
-		if (EventSystem.current.currentSelectedGameObject != null) return;
+        // Deselect all objects
+        DeselectAllObjects();
+    }
 
-		// Shoot a ray from main camera through screen point
-		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    private void SelectObject()
+    {
+        // First check if mouse is currently over UI, do nothing
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+        if (EventSystem.current.currentSelectedGameObject != null) return;
 
-		// Check if ray hits something with the mask "Vehicle"
-		if (Physics.Raycast(ray, out var hit, 100, selectableAreaMasks)) {
-			var hitObject = hit.transform.gameObject;
-			var selectable = hitObject.GetComponent<Selectable>();
+        // Shoot a ray from main camera through screen point
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			// Check if the object is a subclass of Selectable
-			if (selectable != null) {
-				if (!selectable.IsSelected()) {
-					// Select object only if it is not currently selected
-					DeselectAllObjects();
-					selectable.Select();
-					cameraController.SetTarget(hitObject);
-				} else {
-					// If it is currently selected, deselect it and reset camera
-					DeselectAllObjects();
-					cameraController.ResetTarget();
-				}
-			}
-		}
-	}
+        // Check if ray hits something with the mask "Vehicle"
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, selectableAreaMasks))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            Selectable selectable = hitObject.GetComponent<Selectable>();
 
-	private void DeselectAllObjects() {
-		foreach (var obj in gameManager.vehicles) {
-			obj.Deselect();
-		}
-		foreach (var obj in gameManager.various) {
-			obj.Deselect();
-		}
-		foreach (var obj in gameManager.lights) {
-			obj.Deselect();
-		}
+            // Check if the object is a subclass of Selectable
+            if (selectable != null)
+            {
+                if (!selectable.IsSelected())
+                {
+                    // Select object only if it is not currently selected
+                    SetSelectedObject(selectable);
+                }
+                else
+                {
+                    // If it is currently selected, deselect it and reset camera
+                    ResetSelectedObject();
+                }
+            }
+        }
+    }
 
-		// Let camera orbit the default target
-		cameraController.ResetTarget();
-	}
+    private void DeselectAllObjects()
+    {
+        foreach (var obj in gameManager.vehicles)
+        {
+            obj.Deselect();
+        }
+        foreach (var obj in gameManager.various)
+        {
+            obj.Deselect();
+        }
+        foreach (var obj in gameManager.lights)
+        {
+            obj.Deselect();
+        }
 
-	public void MoveSelectedObject() {
-		cameraController.EnableOverviewCamera();
-		selectedObjectMovable = true;
-		selectedObject.GetComponent<Object>().Move();
-		cameraController.UnfollowTarget();
-	}
+        // Let camera orbit the default target
+        cameraController.ResetTarget();
+    }
 
-	private void Move() {
-		Debug.Log(cameraController.CameraIsMoving());
-		if (!cameraController.CameraIsMoving()) {
-			// Shoot ray
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    public void MoveSelectedObject()
+    {
+        cameraController.EnableOverviewCamera();
+        selectedObjectMovable = true;
+        selectedObject.GetComponent<Object>().Move();
+        cameraController.UnfollowTarget();
+    }
 
-			if (Physics.Raycast(ray, out var hit, 100, moveableAreaMask)) {
-				Debug.Log("Ray cast");
-				var destinationPos = new Vector3(
-					Mathf.Ceil(hit.point.x),
-					selectedObject.transform.position.y,
-					Mathf.Ceil(hit.point.z)
-				);
+    private void Move()
+    {
+        Debug.Log(cameraController.CameraIsMoving());
+        if (!cameraController.CameraIsMoving())
+        {
+            // Shoot ray
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-				selectedObject.transform.position = destinationPos;
-			}
-		}
-	}
+            if (Physics.Raycast(ray, out var hit, 100, moveableAreaMask))
+            {
+                Debug.Log("Ray cast");
+                var destinationPos = new Vector3(
+                    Mathf.Ceil(hit.point.x),
+                    selectedObject.transform.position.y,
+                    Mathf.Ceil(hit.point.z)
+                );
 
-	private void PlaceSelectedObject() {
-		// Play audio file
-		soundManager.PlayPlaceObjectSound();
+                selectedObject.transform.position = destinationPos;
+            }
+        }
+    }
 
-		// Disable camera overview mode
-		cameraController.DisableOverviewCamera(selectedObject.gameObject);
+    private void PlaceSelectedObject()
+    {
+        // Play audio file
+        soundManager.PlayPlaceObjectSound();
 
-		// Set object to unmovable
-		selectedObject.GetComponent<Object>().Place();
+        // Disable camera overview mode
+        cameraController.DisableOverviewCamera(selectedObject.gameObject);
 
-		// Set movable boolean to false
-		selectedObjectMovable = false;
+        // Set object to unmovable
+        selectedObject.GetComponent<Object>().Place();
 
-		// Follow the target again
-		cameraController.FollowTarget();
-	}
+        // Set movable boolean to false
+        selectedObjectMovable = false;
 
-	public void RotateSelectedObject() {
-		// Play object rotate sound
-		soundManager.PlayRotateObjectSound();
+        // Follow the target again
+        cameraController.FollowTarget();
+    }
 
-		// Get current rotation
-		Quaternion currentRotation = selectedObject.transform.rotation;
+    public void RotateSelectedObject()
+    {
+        // Play object rotate sound
+        soundManager.PlayRotateObjectSound();
 
-		// Rotate 45 degrees around the y axis
-		selectedObject.transform.rotation = currentRotation * Quaternion.Euler(0, 45.0f, 0);
-	}
+        // Get current rotation
+        Quaternion currentRotation = selectedObject.transform.rotation;
+
+        // Rotate 45 degrees around the y axis
+        selectedObject.transform.rotation = currentRotation * Quaternion.Euler(0, 45.0f, 0);
+    }
 }
