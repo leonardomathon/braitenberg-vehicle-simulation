f190f056f9478a97e0673696fe23a9f4fa14d4ae
Cas Sievers <cassievers@outlook.com>
2021-03-27 15:10:28 +0100

    Added configuration for light bulbs

--- BraitenbergSimulator/Assets/Scripts/Objects/Light/Lightbulb.cs
+++ BraitenbergSimulator/Assets/Scripts/Objects/Light/Lightbulb.cs
@@ -1,6 +1,25 @@
-﻿namespace Objects.Light {
+﻿using System.Collections.Generic;
+using Configurations;
+
+namespace Objects.Light {
 	public class Lightbulb : Object {
 		public float intensity;
 		public float color; // TODO: Possibly replace with fancy emission spectrum if we have time
+		
+		private ConfigurationFloat configureIntensity;
+		
+		public float Intensity {
+			get => intensity;
+			set => intensity = value;
+		}
+
+		private new void Start() {
+			configureIntensity = new ConfigurationFloat("Intensity", "Strength of this light source", () => Intensity, value => Intensity = value);
+		}
+		public override List<Configuration> Configuration() {
+			return new List<Configuration> {
+				configureIntensity
+			};
+		}
 	}
 }
